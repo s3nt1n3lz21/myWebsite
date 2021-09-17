c3d4e326124132196a287015672590efc19c8104
Neil Smith <s3nt1n3lz18@gmail.com>
2021-09-17 12:14:40 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myWebsite';
+
+  showDownloadOptions = false;
+  downloadCVOptions = ['PDF','DOC'];
+  downloadCVOption = "PDF";
+
+  downloadCV(option) {
+    console.log('option: ', option);
+  };
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  value;
+  disabled;
+  vertical;
+  getSliderTickInterval() {}
+  thumbLabel;
+  step;
+  min;
+  max;
+  invert;
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    // console.log(window.scrollY)
+    const header = document.querySelector('.toolbar');
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled'); 
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+// import { MatGridListModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgSelectModule,
+    FormsModule,
+    MatSliderModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    // MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
