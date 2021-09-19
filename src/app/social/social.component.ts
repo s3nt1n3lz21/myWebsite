e0fd740d73f98a224a7946b7ed1b99ea5777107b
Neil Smith <s3nt1n3lz18@gmail.com>
2021-09-19 16:15:56 +0100

    download dropdown now working

---
+++ src/app/social/social.component.ts
@@ -0,0 +1,5 @@
+// TV Time https://www.tvtime.com/en/user/49979554/profile
+// Steam https://steamcommunity.com/profiles/76561198036863778
+// Youtube https://www.youtube.com/channel/UCk5elNWC4Dt5j9Y4ZAwG6fw?view_as=subscriber
+// Twitch https://www.twitch.tv/s3nt1n3lz13
+// Xbox http://live.xbox.com/Profile?Gamertag=s3nt1n3lz13
