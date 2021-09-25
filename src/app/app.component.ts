c5473ffc45c51c7e39f25e207c6cca8c71bbdce7
Neil Smith <s3nt1n3lz18@gmail.com>
2021-09-25 15:03:55 +0100

    Updated CV and increased font sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,13 +49,36 @@ export class AppComponent implements OnInit {
       header.classList.remove('scrolled'); 
     }
   }
+
+  lFollowX = 0;
+  lFollowY = 0;
+  x = 0;
+  y = 0;
+  friction = 1 / 30;
   
   ngOnInit() {
     const el: HTMLElement = document.querySelector(".content");
 
     el.addEventListener("mousemove", (e) => {
-      el.style.setProperty('--x', -e.offsetX + "px");
-      el.style.setProperty('--y', -e.offsetY + "px");
+      console.log('e.offsetX: ', e.offsetX);
+      console.log('e.offsetY: ', e.offsetY);
+
+      let lMouseX = Math.max(-100, Math.min(100, window.outerWidth / 2 - e.clientX));
+      let lMouseY = Math.max(-100, Math.min(100, window.outerHeight / 2 - e.clientY));
+
+      console.log('lMouseX: ', lMouseX);
+      console.log('lMouseY: ', lMouseY);
+
+      let lFollowX = (100 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+      let lFollowY = (100 * lMouseY) / 100;
+
+      let friction;
+
+      this.x += (lFollowX - this.x) * friction;
+      this.y += (lFollowY - this.y) * friction;
+
+      el.style.setProperty('--x', this.x + "px");
+      el.style.setProperty('--y', this.y + "px");
     });
   }
 }
